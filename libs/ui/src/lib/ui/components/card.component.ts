4bae9adcd579db4b80d153642d321679f77b9e9d
dwalase <ramir17@gmail.com>
2025-06-01 07:06:20 +0200

    Feature/card (#1)
    * Global styles
    
    * Card component

--- libs/ui/src/lib/ui/components/card.component.ts
+++ libs/ui/src/lib/ui/components/card.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'lib-card',
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
-export class CardComponent {}
+export class CardComponent {
+  protected readonly imgUrl = input.required<string>();
+  protected readonly header = input.required<string>();
+  protected readonly buttonText = input.required<string>();
+  protected readonly badge = input<string>();
+}
