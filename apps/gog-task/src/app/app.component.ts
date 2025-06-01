8f1c53e20fb41f157fbfceaaf24cfdafc5a9d93f
dwalase <ramir17@gmail.com>
2025-06-01 17:53:17 +0200

    Homepage

--- apps/gog-task/src/app/app.component.ts
+++ apps/gog-task/src/app/app.component.ts
@@ -1,16 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { GameStore } from '@gog-task/store';
-import { GameCardComponent } from './components/game-card/game-card.component';
 
 @Component({
-  imports: [RouterModule, GameCardComponent],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'gog-task';
-
   public store = inject(GameStore);
 }
--- libs/store/src/lib/store/game-store.ts
+++ libs/store/src/lib/store/game-store.ts
@@ -1,26 +1,33 @@
-import { patchState, signalStore, withHooks, withMethods } from '@ngrx/signals';
+import { patchState, signalStore, withComputed, withHooks, withMethods } from '@ngrx/signals';
 import { Game, Games } from '@gog-task/core';
 import { setEntities, updateEntity, withEntities } from '@ngrx/signals/entities';
+import { computed } from '@angular/core';
 
 export const GameStore = signalStore(
   { providedIn: 'root' },
   withEntities<Game>(),
+  withComputed(({ entities }) => ({
+    notFeatured: computed(() =>
+      entities().filter((game) => !game.featured)
+    )
+  })),
   withMethods((store) => ({
-      load() {
-        patchState(store, setEntities(Games))
-      },
-      addToCart(id: number) {
-        patchState(store, updateEntity(
-          {
-            id: id, changes: { ownership: 'InCart' }
-          })
-        )
-      }
-    })
-  ),
+    load() {
+      patchState(store, setEntities(Games));
+    },
+    addToCart(id: number) {
+      patchState(
+        store,
+        updateEntity({
+          id: id,
+          changes: { ownership: 'InCart' },
+        })
+      );
+    },
+  })),
   withHooks({
     onInit(store) {
       store.load();
-    }
-  }),
+    },
+  })
 );
