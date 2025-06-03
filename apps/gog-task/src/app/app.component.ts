e5a9701a257b7382b78c435f50dc34532316b1f1
dwalase <ramir17@gmail.com>
2025-06-03 15:41:56 +0200

    Missing on push

--- apps/gog-task/src/app/app.component.ts
+++ apps/gog-task/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { GameStore } from '@gog-task/store';
 import { MenuComponent } from './components/menu/menu.component';
@@ -8,6 +8,7 @@ import { MenuComponent } from './components/menu/menu.component';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   public store = inject(GameStore);
